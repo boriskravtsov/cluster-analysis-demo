a609cd0a36b510cebf93e1174743568aa340c7cb
Boris Kravtsov <boriskravtsov.contacts@gmail.com>
2025-05-29 12:13:00 +0300

    Add files via upload

---
+++ cluster-analysis-demo/main.py
@@ -0,0 +1,28 @@
+# May-29-2025
+# main.py
+
+from pathlib import Path
+from utils.src.dir_support import files_number_in_directory
+from distances import distances
+from clustering import clustering
+from drawing_of_basket_content import drawing_of_basket_content
+from drawing_of_clusters_content import drawing_of_clusters_content
+
+
+dir_basket = Path.cwd() / 'BASKET'
+n_objects = files_number_in_directory(dir_basket)
+
+print(f'\nC L U S T E R I N G')
+print(f'There are {n_objects} objects in the BASKET folder.')
+input_string = input('Please enter the desired number of clusters: ')
+number_of_clusters = int(input_string)
+
+D, list_basket_paths = distances(dir_basket)
+
+clustering(dir_basket,
+           D,
+           list_basket_paths,
+           number_of_clusters)
+
+all_points = drawing_of_basket_content(dir_basket)
+drawing_of_clusters_content(all_points)
